b8e895a9a9255bc705aa7daf97cb73b3481c73c8
reppc <84331772+reppc@users.noreply.github.com>
2021-11-19 11:04:08 -0600

    examen

--- app/src/main/java/com/example/calculadora/Menu.java
+++ app/src/main/java/com/example/calculadora/Menu.java
@@ -1,64 +1,59 @@
 package com.example.calculadora;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+import com.example.calculadora.Adaptadores.AdaptadorOpciones;
+import com.example.calculadora.Modelos.Opcion;
 
-    Button contactos;
-    Button navegador;
-    Button dialer;
-    Button mapa;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu extends AppCompatActivity {
+    RecyclerView menu;
     String url="https://www.google.com/";
     String urlm="https://www.google.com.mx/maps/preview";
+    Intent c= new Intent(Menu.this, MainActivity.class);
+    Uri _link= Uri.parse(url);
+    Intent n = new Intent(Intent.ACTION_VIEW, _link);
+    Intent d = new Intent(Intent.ACTION_DIAL);
+    Uri _link2= Uri.parse(urlm);
+    Intent m = new Intent(Intent.ACTION_VIEW, _link2);
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        contactos=(Button) findViewById(R.id.esContactos);
-        navegador=(Button) findViewById(R.id.abrirnav);
-        dialer=(Button)findViewById(R.id.dialer);
-        mapa=(Button)findViewById(R.id.mapa);
 
-        mapa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri _link= Uri.parse(urlm);
-                Intent m = new Intent(Intent.ACTION_VIEW, _link);
-                startActivity(m);
-            }
-        });
+        menu= findViewById(R.id.opcionM);
 
-        dialer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    Intent d = new Intent(Intent.ACTION_DIAL);
-                    startActivity(d);
-            }
-        });
+        List<Opcion> lista= new ArrayList<>();
+       lista.add(new Opcion(c));
+        lista.add(new Opcion(n));
+        lista.add(new Opcion(d));
+        lista.add(new Opcion(m));
 
 
-        navegador.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri _link= Uri.parse(url);
-                Intent n = new Intent(Intent.ACTION_VIEW, _link);
-                startActivity(n);
 
-            }
-        });
+        AdaptadorOpciones ap= new AdaptadorOpciones(lista);
+        RecyclerView.LayoutManager opes= new LinearLayoutManager(this);
+        menu.setAdapter(ap);
+        menu.setLayoutManager(opes);
+        menu.setHasFixedSize(true);
 
-        contactos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent c= new Intent(Menu.this, MainActivity.class);
-                startActivity(c);
-            }
-        });
+
+
+
 
     }
+
+
 }
